d5a29c5122f80ef3158701ae4aebe59911e7c370
Andre <andre.agrippa@dcmail.ca>
2021-03-18 09:33:47 -0400

    updated PORT for heroku

--- .history/index_20210318093154.ts
+++ .history/index_20210318093154.ts
@@ -1,32 +1,55 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
+import fs = require('fs');
+import path = require('path');
+// const url = require("url");
+// const http = require('http');
+
+
+//3rd party module
+import express = require('express');
+import { ServerResponse } from 'node:http';
 const app = express();
+
+
+
 const PORT = process.env.PORT || '3000';
 const HOST = "localhost";
+
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use(express.static(path.join(__dirname, "Client")));
 app.use(express.static(path.join(__dirname, "Views")));
+
+// Routing
+//Default route
 app.get('/', (req, res) => {
     displaySPA(res);
-});
-app.get('*', (req, res) => {
-});
-app.listen(PORT, () => {
+  });
+
+  //Wild card route
+  app.get('*', (req, res) =>
+  {
+
+  });
+  
+  app.listen(PORT, () => {
     console.log(`Server running at http://${HOST}:${PORT}/`);
-});
-function displaySPA(res) {
-    fs.readFile("index.html", (err, data) => {
-        if (err) {
+  })
+
+  function displaySPA(res:any):void
+  {
+    fs.readFile("index.html", (err, data) =>
+    {
+        if (err) 
+        {
             res.writeHead(404);
             res.end("ERROR: 404 - Page not found");
             return;
         }
+
         res.writeHead(200);
         res.end(data);
     });
-}
+  }
+
 console.log(`Server running at http://${HOST}:${PORT}/`);
-//# sourceMappingURL=index.js.map
+
+
